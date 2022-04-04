edfd171572883e50eead6158ad54b0774c3de62b
kowany <hvilla65@gmail.com>
2022-04-04 06:57:35 -0600

    setup project

---
+++ src/demo.ts
@@ -0,0 +1,8 @@
+type UserId = string | number;
+let userId: UserId;
+userId = 'string';
+userId = 1;
+console.log('Hello world');
+// ts-node
+// Permite correr typescript desde el lado de node, nos evita
+// estar corriendo el archivo transpilado
